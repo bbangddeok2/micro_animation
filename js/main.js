4cdad714aebcc8fcc80d6dede7c33c29f1ecc006
bbangddeok2 <songseungil39@gmail.com>
2025-11-12 23:06:32 +0900

    1112_오후_인스타 진행중

--- js/main.js
+++ js/main.js
@@ -11,10 +11,18 @@ $(document).ready(function () {
       el: ".swiper-pagination",
     },
   });
+  $(".story_img")
+    .not(".insta_profile .story_img")
+    .click(function () {
+      $(this).toggleClass("active");
+    });
   $(".heart").click(function () {
     $(this).toggleClass("active");
   });
   $(".repost").click(function () {
+    $(this).toggleClass("active");
+  });
+  $(".save").click(function () {
     $(this).toggleClass("active");
   });
 });
