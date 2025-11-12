dc90ed2a71f7922748f400be5c18bf9e5b165840
bbangddeok2 <songseungil39@gmail.com>
2025-11-12 17:17:02 +0900

    1112_카페_작업중
    인스타하는중

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@ $(document).ready(function () {
   $(".hamburger_2").click(function () {
     $(this).toggleClass("active");
   });
+  var swiper = new Swiper(".instaSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  $(".heart").click(function () {
+    $(this).toggleClass("active");
+  });
+  $(".repost").click(function () {
+    $(this).toggleClass("active");
+  });
 });
