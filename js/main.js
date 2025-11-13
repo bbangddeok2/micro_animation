59a9de2abff441f508b27cb46e244c0adbeab7f2
bbangddeok2 <songseungil39@gmail.com>
2025-11-13 12:51:22 +0900

    도는거 작업중

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ $(document).ready(function () {
   $(".hamburger_2").click(function () {
     $(this).toggleClass("active");
   });
+
+  $(".circle_ani_btn").click(function () {
+    $(".circle_ani_mold").toggleClass("active");
+  });
   var swiper = new Swiper(".instaSwiper", {
     pagination: {
       el: ".swiper-pagination",
@@ -25,4 +29,19 @@ $(document).ready(function () {
   $(".save").click(function () {
     $(this).toggleClass("active");
   });
+  $(".notify").click(function () {
+    $(this).toggleClass("active");
+  });
+  $(".follow_btn").click(function () {
+    $(this).toggleClass("active");
+  });
 });
+
+// 변수 0
+
+// 클릭하면 회전한다
+
+// 로테이트0이다
+
+// 클릭할때마다 +36deg
+// 한번클릭하면 36도 돈다
